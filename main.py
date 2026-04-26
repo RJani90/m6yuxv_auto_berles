9fd83aed622218436369f3de86f9c97ceda828a1
Rézsó János <rezsojanos2020@gmail.com>
2026-04-26 17:22:17 +0200

    Company osztály és teljes járműpark létrehozása, és listázása.

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ class Auto(Vehicle):
         return self.__passenger_capacity
 
     def info(self):
-        return f"Car - Plate: {self.license_plate}, Model: {self.make, self.car_type}, Passengers: {self.passenger_capacity}, Price: {self.rental_price} HUF/day"
+        return f"Car - Plate: {self.license_plate} | Model: {self.make} {self.car_type} | Passengers: {self.passenger_capacity} | Price: {self.rental_price} HUF/day"
 
 
 class Truck(Vehicle):
@@ -63,7 +63,7 @@ class Truck(Vehicle):
         return self.__cargo_space
 
     def info(self):
-        return f"Truck - Plate: {self.license_plate}, Model: {self.make,self.car_type}, Payload: {self.__cargo_space} m3, Price: {self.rental_price} HUF/day"
+        return f"Truck - Plate: {self.license_plate} | Model: {self.make} {self.car_type} | Cargo space: {self.__cargo_space} m3 | Price: {self.rental_price} HUF/day"
 
 
 # ==========================================
@@ -89,11 +89,83 @@ class Rental:
         return f"Bérlés rögzítve: {self.vehicle.license_plate} | Dátum: {self.date} | Fizetendő: {self.vehicle.rental_price} HUF"
 
 
-"""Kiíratás"""
+# ==========================================
+# 3. Company osztály (Autókölcsönző)
+# ==========================================
+
+class Company:
+    """A kölcsönzőt és az üzleti logikát kezelő főosztály."""
+
+    def __init__(self, name: str):
+        self.name = name
+        self.__vehicles = []  # Autók (Vehicle objektumok)
+        self.__rentals = []  # Bérlések (Rental objektumok)
+
+    @property
+    def vehicles(self):
+        return self.__vehicles
+
+    @property
+    def rentals(self):
+        return self.__rentals
+
+    def add_vehicle(self, vehicle: Vehicle):
+        """Hozzáad egy új járművet a kölcsönzőhöz."""
+        self.__vehicles.append(vehicle)
+
+    def list_vehicles(self):
+        """Kilistázza az elérhető járműveket."""
+        print(f"\n--- {self.name} Járműparkja ---")
+        for v in self.__vehicles:
+            print(v.info())
+        print("-" * 30)
+
+
+
+"""Kiíratás és Tesztelés"""
 if __name__ == "__main__":
+    # Kölcsönző létrehozása
+    company = Company("OOP Autókölcsönző")
 
-    car1 = Auto(license_plate = "HKR-219", make = "Toyota", car_type = "Corolla", rental_price = 16000, passenger_capacity = 5)
-    truck1 = Truck(license_plate = "TRK-999", make = "Ford", car_type = "Transit", rental_price = 25000, cargo_space = 1.5)
+    # Autók létrehozása
+    car1 = Auto(license_plate="HKR-219", make="Toyota", car_type="Corolla", rental_price=16000, passenger_capacity=5)
+    car2 = Auto(license_plate="JGF-342", make="Skoda", car_type="Octavia", rental_price=18000, passenger_capacity=5)
+    car3 = Auto(license_plate="MBN-555", make="Volkswagen", car_type="Golf", rental_price=15000, passenger_capacity=5)
+    car4 = Auto(license_plate="PTA-112", make="Suzuki", car_type="Vitara", rental_price=17000, passenger_capacity=5)
+    car5 = Auto(license_plate="KJL-987", make="Opel", car_type="Astra", rental_price=13000, passenger_capacity=5)
+    car6 = Auto(license_plate="RWC-404", make="Kia", car_type="Ceed", rental_price=16000, passenger_capacity=5)
+    car7 = Auto(license_plate="TXZ-777", make="Ford", car_type="Focus", rental_price=14000, passenger_capacity=5)
+    car8 = Auto(license_plate="AA-BB-123", make="Hyundai", car_type="Tucson", rental_price=22000, passenger_capacity=5)
 
-    print(car1.info())
-    print(truck1.info())
+    # Teherautók létrehozása
+    truck1 = Truck(license_plate="TRK-999", make="Ford", car_type="Transit", rental_price=25000, cargo_space=1.5)
+    truck2 = Truck(license_plate="LKH-890", make="Mercedes", car_type="Sprinter", rental_price=32000, cargo_space=3.5)
+    truck3 = Truck(license_plate="GTR-221", make="Fiat", car_type="Ducato", rental_price=27000, cargo_space=3.0)
+    truck4 = Truck(license_plate="VBN-444", make="Renault", car_type="Master", rental_price=28000, cargo_space=2.8)
+    truck5 = Truck(license_plate="WQA-101", make="Iveco", car_type="Daily", rental_price=35000, cargo_space=4.2)
+    truck6 = Truck(license_plate="SDF-654", make="Peugeot", car_type="Boxer", rental_price=26000, cargo_space=2.5)
+    truck7 = Truck(license_plate="XCV-998", make="Citroen", car_type="Jumper", rental_price=26000, cargo_space=2.5)
+    truck8 = Truck(license_plate="AA-TC-999", make="Volkswagen", car_type="Crafter", rental_price=31000,cargo_space=3.2)
+
+
+    # Autók hozzáadása a kölcsönzőhöz
+    company.add_vehicle(car1)
+    company.add_vehicle(car2)
+    company.add_vehicle(car3)
+    company.add_vehicle(car4)
+    company.add_vehicle(car5)
+    company.add_vehicle(car6)
+    company.add_vehicle(car7)
+    company.add_vehicle(car8)
+    # Teherautók hozzáadása a kölcsönzőhöz
+    company.add_vehicle(truck1)
+    company.add_vehicle(truck2)
+    company.add_vehicle(truck3)
+    company.add_vehicle(truck4)
+    company.add_vehicle(truck5)
+    company.add_vehicle(truck6)
+    company.add_vehicle(truck7)
+    company.add_vehicle(truck8)
+
+    # A kölcsönző járműparkjának listázása
+    company.list_vehicles()
