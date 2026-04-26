6ad41a96a096379e224585fd8c65d5ef110effbd
Rézsó János <rezsojanos2020@gmail.com>
2026-04-26 11:33:01 +0200

    Truck osztály létrehozás és kiíratás

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 
-class Auto(ABC):
-    def __init__(self, license_plate: str, make: str, type: str, engine: int, cylinder: str, rent: str):
+class Auto:
+    def __init__(self, license_plate: str, make: str, car_type: str, engine: str, cylinder: str, rent: str):
         self.license_plate = license_plate
         self.make = make
-        self.type = type
+        self.car_type = car_type
         self.engine = engine
         self.cylinder = cylinder
         self.rent = rent
@@ -18,9 +18,18 @@ class Auto(ABC):
     def license_plate(self, new_license_plate):
         self._license_plate = new_license_plate
 
-auto1 = Auto("HKR-219","Toyota","Corolla","1,2","Hengerelrendezés: soros","Bérleti díj: 16000")
-print(auto1.license_plate,auto1.make,auto1.type,auto1.engine,auto1.cylinder,auto1.rent)
+class Truck:
+    def __init__(self, license_plate: str, make: str, car_type: str, engine: str, cargo_space: str, rent: str):
+        self.license_plate = license_plate
+        self.make = make
+        self.car_type = car_type
+        self.engine = engine
+        self.cargo_space = cargo_space
+        self.rent = rent
 
-auto1.make = "Jeep"
-print(auto1.make)
+auto1 = Auto("HKR-219","Toyota","Corolla","1,2","In-Line","16000")
+print(auto1.license_plate,auto1.make,auto1.car_type,auto1.engine,auto1.cylinder,auto1.rent)
+
+truck1 = Truck("KGB-123","Iveco","Daily","2,8","Extended","30000")
+print(truck1.license_plate,truck1.make,truck1.car_type,truck1.engine,truck1.cargo_space,truck1.rent)
 
